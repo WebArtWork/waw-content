1ff9ead10dfadaf2e0bd1eeb2f15bb4377c5cbd2
Nika <129855721+nikiquickie@users.noreply.github.com>
2024-07-18 14:17:23 +0300

    Update content.api.js

--- content.api.js
+++ content.api.js
@@ -115,6 +115,7 @@ module.exports = async (waw) => {
 	}
 
 	const contents = await waw.Content.find({});
+	console.log(contents);
 	setTimeout(() => {
 		for (const content of contents) {
 			if(content.url && content.store) {
